78bf42618880d62da097db0aeec6c239028770d7
JasmeetLuthra <jasmeetluthr94@gmail.com>
2020-09-17 23:55:53 +0530

    add courses seeder file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'yaml'
+
+puts "[db:seed] loading courses yaml"
+courses = YAML::load_file "db/default/courses.yml"
+counter = 0
+Course.create!(courses) do |course|
+	puts "Create Course #{counter +=1} with name: #{course.name}"
+end
+
+
+
+
